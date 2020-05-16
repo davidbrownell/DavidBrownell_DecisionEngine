22a76b81de363f65a70698534a11be210069a92f
David Brownell <db@DavidBrownell.com>
2020-05-15 21:49:28 -0700

    Added Condition.h and unit tests

---
+++ src/DecisionEngine/Core/Components/cmake/DecisionEngineCoreComponents.cmake
@@ -0,0 +1,36 @@
+cmake_minimum_required(VERSION 3.5.0)
+
+project(DecisionEngineCoreComponents LANGUAGES CXX)
+
+set(CMAKE_MODULE_PATH "$ENV{DEVELOPMENT_ENVIRONMENT_CMAKE_MODULE_PATH}")
+
+if(NOT WIN32)
+    string(REPLACE ":" ";" CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}")
+endif()
+
+include(BuildHelpers)
+
+function(Impl)
+    include(BoostCommon)
+
+    include(BoostHelpers)
+    include(CommonHelpers)
+
+    get_filename_component(_this_path ${CMAKE_CURRENT_LIST_FILE} DIRECTORY)
+
+    build_library(
+        NAME
+            DecisionEngineCoreComponents
+
+        FILES
+            ${_this_path}/../Condition.cpp
+            ${_this_path}/../Condition.h
+
+        PUBLIC_LINK_LIBRARIES
+            BoostHelpers
+            CommonHelpers
+            ${Boost_LIBRARIES}
+    )
+endfunction()
+
+Impl()
